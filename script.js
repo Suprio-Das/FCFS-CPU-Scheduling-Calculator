dd2306a2f803099949b9c3f1bdb39099d4682d31
Suprio-Das <priyamd507.gmail.com>
2024-03-01 03:17:30 +0600

    Input taken and converted to correct format

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello, buddy!');
+document.getElementById('submit-btn').addEventListener('click', function() {
+    // taking process number
+    const processNumTextOriginal = document.getElementById('process-num');
+    const processNumText = processNumTextOriginal.value.replace(/[^\d.]/g, '');
+
+    // taking arrival time
+    const arrivalTimeText = document.getElementById('arrival-time');
+    // removing extra spaces, new lines etc.
+    const arrivalTimeValue = arrivalTimeText.value.trim();
+    // Split the input value into an array of numbers, filter out empty strings
+    const arrivalTimeArray = arrivalTimeValue.split(/\s+/).filter(Boolean).map(Number);
+
+    // taking burst time
+    const burstTimeText = document.getElementById('burst-time');
+    // removing extra spaces, new lines etc.
+    const burstTimeValue = burstTimeText.value.trim();
+    // Split the input value into an array of numbers, filter out empty strings
+    const burstTimeArray = burstTimeValue.split(/\s+/).filter(Boolean).map(Number);
+
+    // clearing input field
+    arrivalTimeText.value = " ";
+    burstTimeText.value = " ";
+});
